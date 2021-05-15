b42b72f8ab966a9bcdd7cbe76556442e198c12ea
Giordano-Cicchetti <giordano.cicchetti.gc@gmail.com>
2021-05-15 16:31:42 +0200

    cambiato db in postgres

--- db/migrate/20210504212525_add_field_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :nome, :string
-    add_column :users, :cognome, :string
-    add_column :users, :telefono, :string
-  end
-end
