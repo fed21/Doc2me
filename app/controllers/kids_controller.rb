36fe8bea3e8f3402317484365335c0a845ae38da
Giordano-Cicchetti <giordano.cicchetti.gc@gmail.com>
2021-06-11 16:49:03 +0200

    double visit

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -2,7 +2,7 @@ class KidsController < ApplicationController
     def create
         
         @user = User.find(params[:user_id])
-        son = @user.kids.where('kids.name = ? AND kids.surname = ?', params[:kid][:name], params[:kid][:surname])
+        son = @user.kids.where('kids.name = ? AND kids.surname = ?', params[:kid][:name].downcase.titleize, params[:kid][:surname].downcase.titleize)
         
         if son[0] == nil
             @kid = @user.kids.create(kids_params)
@@ -14,17 +14,23 @@ class KidsController < ApplicationController
     end
 
     def destroy
+
       @user = User.find(params[:user_id])
       @kid = @user.kids.find(params[:format])
-      @kid.destroy
-      redirect_to profilo_path(@user)
+      visite = Visit.where(:kid_id => @kid.id)
+      if visite[0] 
+        redirect_to profilo_path(@user), notice: "non puoi eliminare questo figlio poiche ha delle visite prenotate"
+      else 
+        @kid.destroy
+        redirect_to profilo_path(@user)
+      end
     end
     
     private
     
     def kids_params
       p = params.require(:kid).permit(:name,:surname,:sesso, :birth_date, :birth_place)
-      {:name=> p[:name], :surname=>p[:surname], :user_id=>session[:user_id],:sesso=>p[:sesso],
+      {:name=> p[:name].downcase.titleize, :surname=>p[:surname].downcase.titleize, :user_id=>session[:user_id],:sesso=>p[:sesso],
       :birth_date=>p[:birth_date],:birth_place=>p[:birth_place]}
     end
 end
