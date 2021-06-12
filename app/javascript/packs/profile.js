d07e7a85579ff3c548d4a1758ffb85650b285106
fede99ff <federicoandrea.c@gmail.com>
2021-06-12 17:57:27 +0200

    una ultima push

--- app/javascript/packs/profile.js
+++ app/javascript/packs/profile.js
@@ -2,10 +2,8 @@ $(document).ready(function(){
     $(".profilo-card").animate({right: '132.5%'},{
         duration: 2000
     });
-    $("#profile-header").animate({top: '40%'},{
-        duration: 1800
-    });
-    $("#profile-header").animate({left: '33.4%'},{
+    
+    $("#profile-header").animate({left: '70.4%'},{
         duration: 1500
     });
     $("#figli-header").animate({left: '73%'},{
